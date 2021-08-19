aefc4c519b6210ea58643fd438976cf51880de94
guowei-zhang <89202618+guowei-zhang@users.noreply.github.com>
2021-08-19 21:25:42 +0800

    Delete djangoProject directory

--- djangoProject/element.urls.py
+++
@@ -1,8 +0,0 @@
-
-from django.urls import path,include
-from django.views.generic.base import TemplateView
-
-urlpatterns = [
-    path('echarts', TemplateView.as_view(template_name="index.html")),
-
-]
